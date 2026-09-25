acd38b5e27584f7760d44fd93cce383831bd25e8
Ameer <cladnix@gmail.com>
2026-09-24 20:37:02 -0400

    Add main application class and config

---
+++ src/main/java/com/ameeradas/flashcardapp/FlashcardappApplication.java
@@ -0,0 +1,13 @@
+package com.ameeradas.flashcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardappApplication.class, args);
+	}
+
+}
