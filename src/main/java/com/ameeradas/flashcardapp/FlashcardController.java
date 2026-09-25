a778905cc5e42d0e1b5d3414125322acb6cb5d4c
Ameer Adas <cladnix@gmail.com>
2026-09-25 18:26:16 -0400

    Decks now save through restarts to JSON files per username

--- src/main/java/com/ameeradas/flashcardapp/FlashcardController.java
+++ src/main/java/com/ameeradas/flashcardapp/FlashcardController.java
@@ -1,33 +1,75 @@
 package com.ameeradas.flashcardapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FlashcardController {
 
-    private Deck testDeck = new Deck("test1");
-
-    public FlashcardController() {
-        testDeck.addCard(new Flashcard("What class is googer?", "Oni"));
-        testDeck.addCard(new Flashcard("Is googer better than taco?", "Yes"));
-        testDeck.addCard(new Flashcard("What's 2+2", "4"));
-    }
+    private Map<String, Deck> userDecks = new HashMap<>();
+    private ObjectMapper mapper = new ObjectMapper();
+    private String dataFolder = "data";
 
     @GetMapping("/cards")
-    public List<Flashcard> getCards() {
-        return testDeck.getCards();
+    public List<Flashcard> getCards(@RequestParam String user) {
+        Deck deck = loadOrGetDeck(user);
+        return deck.getCards();
     }
 
     @PostMapping("/cards")
-    public String addCard(@RequestParam String question, @RequestParam String answer) {
-    Flashcard newCard = new Flashcard(question, answer);
-    testDeck.addCard(newCard);
-    return "Card added!";
+    public String addCard(@RequestParam String user, @RequestParam String question, @RequestParam String answer) {
+        Deck deck = userDecks.getOrDefault(user, new Deck(user));
+        deck.addCard(new Flashcard(question, answer));
+        saveDeck(user, deck);
+        userDecks.put(user, deck);
+        return "Card added";
+    }
+
+      private Deck loadOrGetDeck(String user) {
+        if (userDecks.containsKey(user)) {
+            return userDecks.get(user);
+        }
+
+        File file = new File(dataFolder + "/" + user + ".json");
+        Deck deck;
+
+        if (file.exists()) {
+            try {
+                Flashcard[] savedCards = mapper.readValue(file, Flashcard[].class);
+                deck = new Deck(user);
+                for (Flashcard card : savedCards) {
+                    deck.addCard(card);
+                }
+            } catch (IOException e) {
+                deck = new Deck(user);
+            }
+        } else {
+            deck = new Deck(user);
+        }
+
+        userDecks.put(user, deck);
+        return deck;
+    }
+
+    private void saveDeck(String user, Deck deck) {
+        try {
+            File folder = new File(dataFolder);
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+            File file = new File(dataFolder + "/" + user + ".json");
+            mapper.writeValue(file, deck.getCards());
+        } catch (IOException e) {
+            System.out.println("Failed to save deck for " + user);
+        }
     }
 }
